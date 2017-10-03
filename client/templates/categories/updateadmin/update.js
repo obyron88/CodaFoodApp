b97fe7745aa3041fa3c044292175c053dd39fb73
obyron88 <coda2017@yahoo.com>
2017-10-03 13:50:28 +0200

    hhhhh

---
+++ client/templates/categories/updateadmin/update.js
@@ -0,0 +1,6 @@
+Meteor.subscribe('pizzas');
+Meteor.subscribe('boissons');
+Meteor.subscribe('burgers');
+Meteor.subscribe('salades');
+Meteor.subscribe('desserts');
+Meteor.subscribe("categories");
